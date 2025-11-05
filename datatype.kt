71e862ec4f9ae2a6fbccad04e73af7fcb8514b96
Abhishree Baral <abhishreebaral4@gmail.com>
2025-11-05 21:12:21 +0545

    Add files via upload

---
+++ datatype.kt
@@ -0,0 +1,20 @@
+package com.example.myproject
+
+
+fun main() {
+
+    val name: String = "Abhishri"
+    val age: Int = 21
+    val height: Double = 5.9
+    val isStudent: Boolean = true
+
+    println("Hello! My name is $name.")
+    println("I am $age years old.")
+    println("My height is $height feet.")
+
+    if (isStudent) {
+        println("I am a student.")
+    } else {
+        println("I am not a student.")
+    }
+}
